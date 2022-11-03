21f11195248d5d1efa190ed5c1bb228b3922a7c0
Ashmyrasheed <ashmytr21@gmail.com>
2022-11-03 09:41:13 +0530

    first

---
+++ bill.py
@@ -0,0 +1,27 @@
+while(True):
+    print("select an option")
+    print("1 Tea")
+    print("2 coffee")
+    print("3 burger")
+    print("4 mandhi")
+    print("5 sandwitch")
+    print("6 generate bill")
+    print("7 exit")
+    choice=int(input("enter the choice"))
+    if(choice==1):
+        print("Added tea")
+    elif(choice==2):
+        print("added coffee")
+    elif(choice==3):
+        print("addedburger")
+    elif(choice==4):
+        print("added mandhi")
+    elif(choice==5):
+        print("added sandwitch")
+    elif(choice==6):
+        print("generating bill")
+    elif(choice==7):
+        break
+        
+        
+    
